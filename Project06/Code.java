a37fc4ae34d0846ab36f2159c9cc8fba53bebf3e
nivdoron1 <102235711+nivdoron1@users.noreply.github.com>
2022-12-14 23:05:34 +0200

    Update Code.java
    changing to static functions

--- Project06/Code.java
+++ Project06/Code.java
@@ -2,70 +2,70 @@ import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Code {
-    private Hashtable<String,String> destTable;
-    private Hashtable<String,String> compTable;
-    private Hashtable<String,String> jumpTable;
+    private static Hashtable<String,String> destTable;
+    private static Hashtable<String,String> compTable;
+    private static Hashtable<String,String> jumpTable;
 
     public Code() {
         this.destTable = new Hashtable<String, String>();
         this.initializationDestTable();
         this.compTable = new Hashtable<String, String>();
-        this.initalizationCompTable();
+        this.initializationCompTable();
         this.jumpTable = new Hashtable<String, String>();
         this.initializationJumpTable();
     }
 
-    private void initializationJumpTable() {
-        this.jumpTable.put("NULL", "000");
-        this.jumpTable.put("JGT", "001");
-        this.jumpTable.put("JEQ", "010");
-        this.jumpTable.put("JGE", "011");
-        this.jumpTable.put("JLT", "100");
-        this.jumpTable.put("JNE", "101");
-        this.jumpTable.put("JLE", "110");
-        this.jumpTable.put("JMP", "111");
+    private static void initializationJumpTable() {
+        jumpTable.put("NULL", "000");
+        jumpTable.put("JGT", "001");
+        jumpTable.put("JEQ", "010");
+        jumpTable.put("JGE", "011");
+        jumpTable.put("JLT", "100");
+        jumpTable.put("JNE", "101");
+        jumpTable.put("JLE", "110");
+        jumpTable.put("JMP", "111");
     }
 
-    private void initalizationCompTable() {
-        this.compTable.put("0", "0101010");
-        this.compTable.put("1", "0111111");
-        this.compTable.put("-1", "0111010");
-        this.compTable.put("D", "0001100");
-        this.compTable.put("A", "0110000");
-        this.compTable.put("M", "1110000");
-        this.compTable.put("!D", "0001101");
-        this.compTable.put("!A", "0110001");
-        this.compTable.put("!M", "1110001");
-        this.compTable.put("-D", "0001111");
-        this.compTable.put("-A", "0110011");
-        this.compTable.put("-M", "1110011");
-        this.compTable.put("D+1", "0011111");
-        this.compTable.put("A+1", "0110111");
-        this.compTable.put("M+1", "1110111");
-        this.compTable.put("D-1", "0001110");
-        this.compTable.put("A-1", "0110010");
-        this.compTable.put("M-1", "1110010");
-        this.compTable.put("D+A", "0000010");
-        this.compTable.put("D+M", "1000010");
-        this.compTable.put("D-A", "0010011");
-        this.compTable.put("D-M", "1010011");
-        this.compTable.put("A-D", "0000111");
-        this.compTable.put("M-D", "1000111");
-        this.compTable.put("D&A", "0000000");
-        this.compTable.put("D&M", "1000000");
-        this.compTable.put("D|A", "0010101");
-        this.compTable.put("D|M", "1010101");
+    private static void initializationCompTable() {
+        compTable.put("0", "0101010");
+        compTable.put("1", "0111111");
+        compTable.put("-1", "0111010");
+        compTable.put("D", "0001100");
+        compTable.put("A", "0110000");
+        compTable.put("M", "1110000");
+        compTable.put("!D", "0001101");
+        compTable.put("!A", "0110001");
+        compTable.put("!M", "1110001");
+        compTable.put("-D", "0001111");
+        compTable.put("-A", "0110011");
+        compTable.put("-M", "1110011");
+        compTable.put("D+1", "0011111");
+        compTable.put("A+1", "0110111");
+        compTable.put("M+1", "1110111");
+        compTable.put("D-1", "0001110");
+        compTable.put("A-1", "0110010");
+        compTable.put("M-1", "1110010");
+        compTable.put("D+A", "0000010");
+        compTable.put("D+M", "1000010");
+        compTable.put("D-A", "0010011");
+        compTable.put("D-M", "1010011");
+        compTable.put("A-D", "0000111");
+        compTable.put("M-D", "1000111");
+        compTable.put("D&A", "0000000");
+        compTable.put("D&M", "1000000");
+        compTable.put("D|A", "0010101");
+        compTable.put("D|M", "1010101");
     }
 
-    private void initializationDestTable() {
-        this.destTable.put("NULL", "000");
-        this.destTable.put("M", "001");
-        this.destTable.put("D", "010");
-        this.destTable.put("MD", "011");
-        this.destTable.put("A", "100");
-        this.destTable.put("AM", "101");
-        this.destTable.put("AD", "110");
-        this.destTable.put("AMD", "111");
+    private static void initializationDestTable() {
+        destTable.put("NULL", "000");
+        destTable.put("M", "001");
+        destTable.put("D", "010");
+        destTable.put("MD", "011");
+        destTable.put("A", "100");
+        destTable.put("AM", "101");
+        destTable.put("AD", "110");
+        destTable.put("AMD", "111");
     }
 
     public String binary(String input) {
@@ -80,22 +80,27 @@ public class Code {
     }
 
 
-    public String jump(String key) {
+    public static String jump(String key) {
+        initializationJumpTable();
         if (key == null || key.isEmpty()) {
             key = "NULL";
         }
-
-        return this.jumpTable.get(key);
+        String out = jumpTable.get(key);
+        return out;
     }
 
-    public String comp(String key) {
-        return this.compTable.get(key);
+    public static String comp(String key) {
+        initializationCompTable();
+        String out = compTable.get(key);
+        return out;
     }
 
-    public String dest(String key) {
+    public static String dest(String key) {
+        initializationDestTable();
         if (key == null || key.isEmpty()) {
             key = "NULL";
         }
-        return this.destTable.get(key);
+        String out = destTable.get(key);
+        return out;
     }
 }

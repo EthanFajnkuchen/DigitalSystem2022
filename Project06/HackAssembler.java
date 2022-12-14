53a0db410eaba3e85d221996f919692d3c6f58a8
nivdoron1 <102235711+nivdoron1@users.noreply.github.com>
2022-12-14 11:39:31 +0200

    Update HackAssembler.java
    adding the parser class

--- Project06/HackAssembler.java
+++ Project06/HackAssembler.java
@@ -1,11 +1,114 @@
-import java.nio.charset.StandardCharsets;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 public class HackAssembler {
+    public class Parser {
+        public static final int COMMAND_A = 1;
+        public static final int COMMAND_C = 2;
+        public static final int COMMAND_L = 3;
+
+
+        private BufferedReader bfrReader;
+        private String current;
+        private String next;
+
+        public Parser(File input) throws IOException {
+            this.bfrReader = new BufferedReader(new FileReader(input));
+            this.current = null;
+            this.next = this.readNextLine();
+        }
+
+        private boolean comment(String input) {
+            boolean isComment = false;
+            isComment = input.trim().startsWith("//");
+            return isComment;
+        }
+
+        public boolean hasMoreLine() {
+            if (this.next != null) {return true;}
+            return false;
+        }
+
+        public String readNextLine() throws IOException {
+            try {
+                String nextLine = "";
+                nextLine = this.bfrReader.readLine();
+                if (nextLine == null) {
+                    return null;
+                }
+                while (nextLine.trim().isEmpty() || this.comment(nextLine)) {
+                    nextLine = this.bfrReader.readLine();
+                    if (nextLine == null) {
+                        return null;
+                    }
+                }
+                if (nextLine.indexOf("//") != -1) {
+                    nextLine = nextLine.substring(0, nextLine.indexOf("//") - 1);
+                }
+                return nextLine;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public void advance() throws IOException {
+            this.current = this.next;
+            this.next = this.readNextLine();
+        }
+
+        public int instructionType() {
+            String line = this.current.trim();
+            if (line.startsWith("(") && line.endsWith(")")) {return COMMAND_L; }
+            if (line.startsWith("@")) { return COMMAND_A;}
+            return COMMAND_C;
+        }
+
+        public String symbol() {
+            String line = this.current.trim();
+            if (this.instructionType() == COMMAND_L) { return line.substring(1,this.current.length() - 1); }
+            if (this.instructionType() == COMMAND_A) { return line.substring(1);}
+            return null;
+        }
+
+        public String dest() {
+            String line = this.current.trim();
+            if (line.indexOf("=") == -1) {
+                return null;
+            } else {
+                return line.substring(0,line.indexOf("="));
+            }
+        }
+
+        public String comp() {
+            String line = this.current.trim();
+            if (line.indexOf("=") == -1) {
+                line = line.substring(line.indexOf("=") + 1);
+            }
+
+            if (line.indexOf(";") == -1) {
+                return line;
+            } else {
+                return line.substring(0,line.indexOf(";"));
+            }
+        }
+
+        public String jump() {
+            String line = this.current.trim();
+            if (line.indexOf(";") == -1) {
+                return null;
+            } else {
+                return line.substring(line.indexOf(";") + 1);
+            }
+        }
+    }
     public class Code {
         private Hashtable<String,String> destTable;
         private Hashtable<String,String> compTable;
         private Hashtable<String,String> jumpTable;
-        
+
         //making the constructor
         public Code() {
             this.destTable = new Hashtable<String, String>();
@@ -88,12 +191,12 @@ public class HackAssembler {
 
             return this.jumpTable.get(key);
         }
-        
+
         // return the comp from the dictionaries
         public String comp(String key) {
             return this.compTable.get(key);
         }
-        
+
         // return the destination from the dictionaries
         public String dest(String key) {
             if (key == null || key.isEmpty()) {
@@ -101,7 +204,7 @@ public class HackAssembler {
             }
             return this.destTable.get(key);
         }
-        
+
         // to string function
         public String toString()
         {
